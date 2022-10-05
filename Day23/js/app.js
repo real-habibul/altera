8063807c583fd3808060d72b0b318d88d3e7081f
Ahmad Habibul Ulum <ahmadhabibululum@gmail.com>
2022-10-05 15:20:42 +0700

    Day 23 - Fundamental Typescript

---
+++ Day23/js/app.js
@@ -0,0 +1,6 @@
+"use strict";
+var nama = "Ahmad Habibul Ulum";
+var kota = "Mojokerto";
+console.log("Hello!!");
+console.log("Nama saya adalah " + nama);
+console.log("Saya tinggal di kota " + kota);
