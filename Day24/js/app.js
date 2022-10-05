5b7c63369eca13ec7b3a5fdeb4e5ad339768a5cf
Ahmad Habibul Ulum <ahmadhabibululum@gmail.com>
2022-10-05 15:30:01 +0700

    DELETE

--- Day24/js/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var nama = "Ahmad Habibul Ulum";
-var kota = "Mojokerto";
-console.log("Hello!!");
-console.log("Nama saya adalah " + nama);
-console.log("Saya tinggal di kota " + kota);
